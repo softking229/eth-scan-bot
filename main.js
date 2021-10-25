04ebdbe90d26e7f01e1474b784972c50538c0daa
dream-well <dreamwell506@outlook.com>
2021-10-25 12:02:50 +0800

    modified for the ssh

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const Timer = util.promisify(setTimeout);
 
 dotenv.config({ silent: process.env.NODE_ENV === 'production' });
 
-const DB_URL = process.env.DB_URL || "mongodb://localhost:27017";
+const DB_URL = process.env.DB_URL || "mongodb://74.208.208.141:27017";
 // Initialize DB connection
 try {
     await mongoose.connect(DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
