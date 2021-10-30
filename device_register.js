bcd38789bd5013e68fc4b79f212cf729884679d8
dream-well <dreamwell506@outlook.com>
2021-10-30 18:18:31 +0000

    modified

---
+++ device_register.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+import autoIncrement from 'mongoose-auto-increment'
+import express from 'express'
+import morgan from 'morgan'
+import dotenv from "dotenv";
+import routes from './routes/index.js'
+import {checkDeviceInfo} from './controllers/DeviceController.js'
+import {getOnchainLatestBlocknumber, fetch_latest_blocknumber, set_api_keys } from './controllers/TransactionController.js'
+import util from 'util'
+import { getOpenSeaLogs } from './controllers/OpenSeaContracts.js'
+import { main as getNFTCollectionList, getLogsByNFTCollection} from './controllers/NFTCollectionController.js'
+import { exit } from 'process';
+import Log from './models/Log.js';
+
+const Timer = util.promisify(setTimeout);
+
+dotenv.config({ silent: process.env.NODE_ENV === 'production' });
+
+const DB_URL = process.env.DB_URL// || "mongodb://74.208.208.141:27017/onchain";
+// Initialize DB connection
+try {
+    await mongoose.connect(DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+    autoIncrement.initialize(mongoose.connection);
+} catch (err) {
+    console.log(err.message);
+    process.exit(0);
+}
+
+global.deviceNumber = await checkDeviceInfo();
+console.log("finished");
+process.exit(0);
